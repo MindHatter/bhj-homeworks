fa5a79e9810a717b234cd503777eaaa0eb9dbda3
Aleksandr Menshikov <menshikov.abz@gmail.com>
2021-01-05 21:11:25 +0300

    'async-requests' task 'preload'  is fixed

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -8,18 +8,21 @@ xhr.open('GET', 'https://netology-slow-rest.herokuapp.com');
 xhr.onreadystatechange = function () {
     if (xhr.readyState === xhr.DONE) {
         let data = JSON.parse(this.responseText).response.Valute;
-        for (key in data) {
-            items.insertAdjacentHTML('beforeend', `<div class="item">
+        console.log(data);
+        for (let key in data) {
+            console.log(key);
+            items.insertAdjacentHTML('beforeend', 
+            `<div class="item">
             <div class="item__code">
-            ${data[key].CharCode}
-        </div>
-        <div class="item__value">
-            ${data[key].Value}
-        </div>
-        <div class="item__currency">
-            руб.
-        </div>
-        </div>`)
+                ${data[key].CharCode}
+            </div>
+            <div class="item__value">
+                ${data[key].Value}
+            </div>
+            <div class="item__currency">
+                руб.
+            </div>
+            </div>`)
         }
     }
     loader.classList.remove('loader_active');
